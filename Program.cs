a0d8e78bce5e2b955df8d4d3ecc3c294a4354fc1
MitayiSana <mitayisana@gmail.com>
2024-10-17 13:39:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace TupleToDictionary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Tuple Elements......
+            Tuple <int,string> tuple1 = new Tuple<int,string>(1,"Diya");
+            Console.WriteLine(tuple1);
+            Tuple<string, int, string> tuple2 = new Tuple<string, int, string>("Software", 2, "Sana");
+            Console.WriteLine(tuple2);
+
+            //Creating empty dictionary Empty dictionary.....
+            Dictionary <int,string>dict = new Dictionary<int,string>();
+            dict.Add(tuple1.Item1, tuple1.Item2);
+            foreach(KeyValuePair<int,string> kvp in dict)
+            {
+                Console.WriteLine($"{kvp.Key}:{kvp.Value}");
+            }
+            //Second dictionary.......
+            Dictionary<string, int> dict1 = new Dictionary<string, int>();
+            dict1.Add(tuple2.Item1, tuple2.Item2);
+            foreach (KeyValuePair<string, int> kvp in dict1)
+            {
+                Console.WriteLine($"{kvp.Key}:{kvp.Value}");
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
